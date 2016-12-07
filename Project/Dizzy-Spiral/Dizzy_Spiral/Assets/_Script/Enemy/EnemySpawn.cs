faccab09e768f36618733a01a8c53175eb0fe6c2
PICHLER\Michael Pichler <1812michaelpichler@gmx.at>
2016-12-07 13:11:42 +0100

    First Try

---
+++ Project/Dizzy-Spiral/Dizzy_Spiral/Assets/_Script/Enemy/EnemySpawn.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemySpawn : MonoBehaviour {
+
+    private PlayerMovement playerMovement;
+
+    public List<GameObject> enemyObject = new List<GameObject>();
+    public List<float> randomBoundary = new List<float>();
+
+    public float timeNeedToSpawn = 3.0f;
+    public float minSpeed = 90.0f;
+    public float maxSpeed = 720.0f;
+    //public float nextSpawnStart = 4.0f;
+
+    public float enemyLife = 5.0f;
+
+    private float currentTime = 0.0f;
+
+    Vector3 pos;
+    // Use this for initialization
+    void Start () {
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+
+        setPosition();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+        transform.Rotate(new Vector3(0.0f, currentTime / timeNeedToSpawn * (maxSpeed - minSpeed) * Time.deltaTime, 0.0f));
+
+        if(currentTime >= timeNeedToSpawn)
+        {
+            float randNumber = Random.Range(0.0f, 1.0f);
+
+            int i = 0;
+
+            for(int j=0; j < randomBoundary.Count; ++j)
+            {
+                if(randNumber <= randomBoundary[j])
+                {
+                    i = j;
+                    j = randomBoundary.Count;
+                }
+            }
+
+            GameObject o = (GameObject)GameObject.Instantiate(enemyObject[i], pos, Quaternion.identity);
+            o.GetComponent<SimpleEnemy>().LifeTime = enemyLife;
+
+            if (i == 1)
+                o.GetComponent<Rigidbody>().AddForce(new Vector3(30.0f, 0.0f, 0.0f));
+
+            transform.Translate(new Vector3(0.0f, -5.0f, 0.0f), Space.World);
+
+            currentTime = 0.0f;
+            setPosition();
+        }
+	}
+
+    private void setPosition()
+    {
+        float r = Random.Range(playerMovement.MinRadius, playerMovement.MaxRadius);
+        float angle = Random.Range(0.0f, Mathf.PI * 2.0f);
+
+        pos = new Vector3(r * Mathf.Sin(angle), 2.0f, r * Mathf.Cos(angle));
+        transform.position = pos;
+    }
+}
---
+++ Project/Dizzy-Spiral/Dizzy_Spiral/Assets/_Script/Enemy/SimpleEnemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleEnemy : MonoBehaviour {
+
+    private HighscoreScript highScoreScript = null;
+    
+    public float damage = 100.0f;
+
+    private float lifeTime = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        highScoreScript = HighscoreScript.Instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeTime -= Time.deltaTime;
+
+        if(lifeTime <= 0.0f)
+        {
+            GameObject.Destroy(transform.gameObject);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "PlayerObject")
+            highScoreScript.addPoints((int)(-1*damage));
+    }
+
+    public float LifeTime
+    {
+        get { return lifeTime; }
+        set { lifeTime = value; }
+    }
+}
