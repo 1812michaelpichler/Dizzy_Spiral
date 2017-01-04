b4e04ec0b69e33c11414a9cd91c863aea398b242
PICHLER\Michael Pichler <1812michaelpichler@gmx.at>
2017-01-04 10:10:28 +0100

    Prepare for saving

---
+++ Project/Dizzy-Spiral/Dizzy_Spiral/Assets/_Script/Highscore/HighscoreList.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[System.Serializable]
+public class HighscoreList {
+
+    private int maxItems = 5;
+
+    private List<string> nameList = new List<string>();
+    private List<int> highscoreList = new List<int>();
+
+    //returns true if score is in the top <maxItems>
+    public bool addElement(string name, int score)
+    {
+        if(highscoreList.Count < maxItems)
+        {
+            nameList.Add(name);
+            highscoreList.Add(score);
+        }
+
+        int minIndex = findMin();
+
+        if(highscoreList[minIndex] < score)
+        {
+            highscoreList[minIndex] = score;
+            nameList[minIndex] = name;
+        }
+
+        sortList();
+
+        return false;
+    }
+
+    private int findMin()
+    {
+        int minIndex = 0;
+
+        for(int i=1; i<highscoreList.Count; ++i)
+        {
+            if (highscoreList[i] < highscoreList[minIndex])
+                minIndex = i;
+        }
+
+        return minIndex;
+    }
+
+    private void sortList()
+    {
+        for(int i=0; i<highscoreList.Count; ++i)
+        {
+            for(int j=1; j<highscoreList.Count; ++j)
+            {
+                if(highscoreList[j] < highscoreList[i])
+                {
+                    int scoreTmp = highscoreList[i];
+                    string nameTmp = nameList[i];
+
+                    highscoreList[i] = highscoreList[j];
+                    nameList[i] = nameList[j];
+
+                    highscoreList[j] = scoreTmp;
+                    nameList[j] = nameTmp;
+                }
+            }
+        }
+    }
+    
+    public byte[] getBinaryData()
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        MemoryStream ms = new MemoryStream();
+        binaryFormatter.Serialize(ms, this);
+        byte[] b = ms.ToArray();
+
+        return b;
+    }
+}
--- Project/Dizzy-Spiral/Dizzy_Spiral/Assets/_Script/Menu/SaveHighscoreMenuHandler.cs
+++ Project/Dizzy-Spiral/Dizzy_Spiral/Assets/_Script/Menu/SaveHighscoreMenuHandler.cs
@@ -2,18 +2,31 @@
 using System.Collections;
 using UnityEngine.UI;
 
+using System.IO;
 public class SaveHighscoreMenuHandler : MonoBehaviour {
-    
+
+    private string filename = "DizzyHighscore";
+
+    private HighscoreList highscoreList;
+
+    private FileInfo file;
+
     void Start()
     {
+        highscoreList = new HighscoreList();
     }
 
     public void handleSaveHighscore()
     {
         transform.parent.FindChild("ShowHighscoreMenu").gameObject.SetActive(true);
-        /******
-        //Save code
-        *****/
+
+#if UNITY_ANDROID
+        saveOnAndroid();
+#else
+        saveOnPC();
+#endif
+
+
         HighscoreScript.Instance.Highscore = 0;
         this.gameObject.SetActive(false);
     }
@@ -23,6 +36,37 @@ public class SaveHighscoreMenuHandler : MonoBehaviour {
         Destroy(GameObject.Find("PlayableObjects(Clone)"));
 
         Text t = transform.Find("InfoText").GetComponent<Text>();
-        t.text = "Game Over\nYour Points:\n" + HighscoreScript.Instance.Highscore;
+
+        bool isIn =  highscoreList.addElement("name", HighscoreScript.Instance.Highscore);
+
+        if(isIn)
+            t.text = "Game Over\nYour Points:\n" + HighscoreScript.Instance.Highscore + "\nYou are in the list :)";
+        else
+            t.text = "Game Over\nYour Points:\n" + HighscoreScript.Instance.Highscore + "\nYou are not in the list :(";
+    }
+
+    private void saveOnAndroid()
+    {
+        byte[] b = highscoreList.getBinaryData();
+
+        file = new FileInfo(Application.persistentDataPath + "/" + "name" + ".txt");
+        StreamWriter w;
+        if (!file.Exists)
+        {
+            w = file.CreateText();
+        }
+        else
+        {
+            file.Delete();
+            w = file.CreateText();
+        }
+
+        w.WriteLine(b);
+        w.Close();
+    }
+
+    private void saveOnPC()
+    {
+        byte[] b = highscoreList.getBinaryData();
     }
 }
