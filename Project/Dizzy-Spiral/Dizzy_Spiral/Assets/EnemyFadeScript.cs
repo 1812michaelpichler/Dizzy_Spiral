57c399b6f99684990a51650659d48791d00d145e
PICHLER\Michael Pichler <1812michaelpichler@gmx.at>
2017-01-25 20:15:48 +0100

    Faded change

--- Project/Dizzy-Spiral/Dizzy_Spiral/Assets/EnemyFadeScript.cs
+++ Project/Dizzy-Spiral/Dizzy_Spiral/Assets/EnemyFadeScript.cs
@@ -6,6 +6,7 @@ public class EnemyFadeScript : MonoBehaviour {
 	private bool fade = false;
 
 	public float speed = 1.0f;
+    public float maxAlpha = 0.45f;
 
 	private bool halfway = false;
 
@@ -15,10 +16,39 @@ public class EnemyFadeScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 		if (fade) {
-			float lerp = Mathf.PingPong(Time.time, speed) / speed;
+            
+
+            Color color = transform.GetComponent<MeshRenderer>().material.color;//SetColor("color", new Color(1,0,0,alpha));
+            float alpha = 0.0f;
+
+            if (!halfway)
+            {
+                alpha = color.a;
+
+                alpha += (speed * Time.fixedDeltaTime)/255.0f;
+
+                if (alpha > maxAlpha)
+                    halfway = true;
+            }
+            else
+            {
+                alpha = color.a;
+
+                alpha -= (speed * Time.fixedDeltaTime)/255.0f;
+
+                if (alpha <= 0)
+                {
+                    halfway = false;
+                    fade = false;
+                }
+            }
+
+            color.a = alpha;
+            transform.GetComponent<MeshRenderer>().material.color = color;
+            /*float lerp = Mathf.PingPong(Time.time, speed) / speed;
 
 			float alpha = Mathf.Lerp (0.0f, 1.0f, lerp);
 
@@ -37,8 +67,8 @@ public class EnemyFadeScript : MonoBehaviour {
 				Color color2 = transform.GetComponent<MeshRenderer>().material.color;//SetColor("color", new Color(1,0,0,alpha));
 				color2.a = 0;
 				transform.GetComponent<MeshRenderer> ().material.color = color2;
-			}
-		}
+			}*/
+        }
 
 	}
 
